150ad0e333749ca36a8961b0b41d35dea2515901
Blacks7566 <nitinsoni815@gmail.com>
2023-01-22 17:54:22 +0530

    try to create portfolio

---
+++ static/main.js
@@ -0,0 +1,14 @@
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamCross').addEventListener("click", () => {
+
+    document.querySelector('.sideBar').classList.toggle('sideBarGo');
+
+    if (document.querySelector('.sideBar').classList.contains('sideBarGo')) {
+        document.querySelector('.ham').style.display = "inline"
+        document.querySelector('.cross').style.display = "none"
+    } else {
+
+        document.querySelector('.ham').style.display = "none"
+        document.querySelector('.cross').style.display = "inline"
+    }
+})
